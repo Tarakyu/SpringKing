1bedeb3248e6bd87386a0970e82f40a1e798d685
Tarakyu <worrasong@gmail.com>
2022-01-12 20:08:43 +0900

    220112 JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,12 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
     private String name;
 
