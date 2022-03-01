7b476fb94b446c53b2822ec8f599502e9dc0f011
Tarakyu <worrasong@gmail.com>
2022-03-01 17:15:43 +0900

    feat: create member repository with spring data JPA

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,29 +1,27 @@
 package study.datajpa.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback(false)
-class MemberJpaRepositoryTest {
+class MemberRepositoryTest {
 
-    @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
-    public void testMember(){
+    public void testMember() {
         Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
+        Member savedMember = memberRepository.save(member);
 
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
