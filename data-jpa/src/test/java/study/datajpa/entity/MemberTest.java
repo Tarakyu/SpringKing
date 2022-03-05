d47aa3aee6284148afe6254a6779d7cc8ca83c33
Tarakyu <worrasong@gmail.com>
2022-03-05 17:09:21 +0900

    feat: add base entity with JPA

--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,8 +1,10 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,6 +20,8 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -47,5 +51,25 @@ class MemberTest {
             System.out.println("-> member.Team = " + member.getTeam());
         }
 
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist 작동
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); // @PreUpdate
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println(findMember.getCreatedDate());
+        System.out.println(findMember.getUpdatedDate());
     }
 }
